7ab639ec811e82af9e4d587deb68166c3e780902
kaan colak <c.kaaan@gmail.com>
2022-05-03 22:15:01 +0300

    js 1.ders

---
+++ src/app.js
@@ -0,0 +1,18 @@
+let dolardun=9.20
+let dolarBugun=9.30
+console.log(dolarBugun)
+
+const euroDun=11.2
+console.log(euroDun)
+
+let konutKredileri=["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+konutKredileri[i]+"</li>")
+    
+}
+
+console.log("</ul>")
+
+console.log(konutKredileri)
